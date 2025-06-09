91e6c29c631c0b2b7bc190eb5e17ddac324e04a2
yabca1 <yabetsgobeze95@gmail.com>
2025-06-09 03:23:06 +0300

    set scale and dividing my canvas
    when i divide the canvas height and width by the scale i get 25 rows and columns

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const canvas=document.queryselector(".canvas");
 const ctx= canvas.getcontext("2d");
+
+
+console.log(ctx);
+//to be able control the movt of our snake in a control way
+// lets devide our canvas into 10 by 10 small squares
+const scale=20;
+const rows= canvas.height/scale;
+const columns= canvas.width/ scale;
